0d21dccba5100fd393642facc289aaacd9ab3300
marinov752 <mbm.marinov@gmail.com>
2025-12-02 03:41:22 +0200

    test: fix deprecated MockBean annotations and unused imports

--- src/test/java/com/questanalytics/controller/QuestAnalyticsControllerApiTest.java
+++ src/test/java/com/questanalytics/controller/QuestAnalyticsControllerApiTest.java
@@ -4,8 +4,7 @@ import com.questanalytics.service.QuestAnalyticsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.HashMap;
@@ -23,7 +22,7 @@ class QuestAnalyticsControllerApiTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private QuestAnalyticsService questAnalyticsService;
 
     @Test
